19aa028d44b445e8a5f1ad3cea857897a7bdcc6d
pevzner47 <pevzner47@gmail.com>
2019-10-24 08:48:17 +0300

    Добавил миграцию для correct

--- test-guru/db/schema.rb
+++ test-guru/db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_080839) do
+ActiveRecord::Schema.define(version: 2019_10_24_054451) do
 
   create_table "answers", force: :cascade do |t|
-    t.boolean "correct", default: false
+    t.boolean "correct", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "question_id"
