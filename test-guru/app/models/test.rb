029ec498aca8a5337e4f7ee6e4810f4d978b70bb
pevzner47 <pevzner47@gmail.com>
2019-10-30 14:33:03 +0300

    очень сыро, но слишком много вопросов

--- test-guru/app/models/test.rb
+++ test-guru/app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+  class << self
+    def all_tests_in_category(category_name)
+      pp Test.where('title: ?', category_name).order('title DESC')
+    end
+  end
 end
--- test-guru/db/seeds.rb
+++ test-guru/db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  categories = Category.create([
+                                {title: 'Ruby'},
+                                {title: 'HTML'},
+                                {title: 'JS'},
+                                {title: 'CSS'}])
+
+  tests = Test.create([{ title: 'Ruby', level: 0, category_id: 1 },
+                      { title: 'Ruby on Rails', level: 1, category_id: 1 },
+                      { title: 'HTML', level: 0, category_id: 2 },
+                      { title: 'HTML 5', level: 1, category_id: 2 },
+                      { title: 'JS', level: 0, category_id: 3 },
+                      { title: 'CSS', level: 0, category_id: 4 }])
+
+  questions = Question.create([{ text: 'Класс это...', answer: 'Объект'},
+                             { text: 'Объект это...', answer: 'Класс'}])
+
+  answers = Answer.create([{ correct: false, question_id: 1},
+                           { correct: false, question_id: 2}])
+
+  users = User.create([{ name: 'Beginer'}])
