648c357f26161a696dbf63e850f8845f10c2a602
pevzner47 <pevzner47@gmail.com>
2019-11-03 00:35:47 +0300

    исправил замечания в pr

--- test-guru/app/models/user.rb
+++ test-guru/app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   has_many :tests, through: :users_tests
 
   def passes_tests(level)
-    tests = Test.joins("JOIN users_tests ON users_tests.user_id = #{ self.id.to_s} AND users_tests.test_id = tests.id").where('tests.level = ?', level)
-                                                                # ^  и здесь не смог через "?"
+    Test.joins("JOIN users_tests ON users_tests.test_id = tests.id").where(tests: { level: level}, users_tests: { user_id: self.id})
   end
 end
