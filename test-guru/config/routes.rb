0daad31d4b951e778bb50d54976abb94fb6a9149
pevzner47 <pevzner47@gmail.com>
2019-11-21 11:14:29 +0300

    to merge

--- test-guru/config/routes.rb
+++ test-guru/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'tests#index'
 
   resources :tests do
-    resources :questions, only: [:show, :create, :new, :destroy, :edit, :update], shallow: true
+    resources :questions, except: :index, shallow: true
   end
 
   get '/tests/:category/:title', to: 'tests#search'
